435126c94506e9416cb8bc9c1a9df3c7c5fd9a7a
Silesh K Nair <sileshn@gmail.com>
2015-09-17 09:40:56 +0530

    Add OTA properties

--- cm.mk
+++ cm.mk
@@ -29,6 +29,12 @@ PRODUCT_NAME := cm_meliusltexx
 PRODUCT_MODEL := GT-I9205
 PRODUCT_MANUFACTURER := samsung
 
+# OTA
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.ota.romname=temasek-melius \
+ro.ota.version=$(shell date +%F | sed s@-@@g) \
+ro.ota.manifest=https://dl.dropboxusercontent.com/s/ak7xqv7g9t7yl5k/temasek-melius.xml?dl=1
+
 # override
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="meliusltexx-user 4.4.2 KOT49H I9205XXUDNE4 release-keys" \
